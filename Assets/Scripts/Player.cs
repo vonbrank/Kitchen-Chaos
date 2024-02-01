1a73e2bd020e779b9811620b765c49fd5049adcd
vonbrank <vonbrank@outlook.com>
2024-02-01 22:00:13 +0800

    feat: 实现角色碰撞检测

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,9 +15,54 @@ namespace DefaultNamespace
 
             var moveDir = new Vector3(inputVector.x, 0, inputVector.y);
 
-            IsWalking = moveDir != Vector3.zero;
+            float moveDistance = moveSpeed * Time.deltaTime;
+            float playerRadius = 0.7f;
+            float playerHeight = 2f;
 
-            transform.position += moveDir * (moveSpeed * Time.deltaTime);
+            bool canMove = !Physics.CapsuleCast(
+                transform.position,
+                transform.position + Vector3.up * playerHeight,
+                playerRadius,
+                moveDir,
+                moveDistance);
+            if (!canMove)
+            {
+                var moveDirX = new Vector3(inputVector.x, 0, 0).normalized;
+                canMove = !Physics.CapsuleCast(
+                    transform.position,
+                    transform.position + Vector3.up * playerHeight,
+                    playerRadius,
+                    moveDirX,
+                    moveDistance);
+                if (canMove)
+                {
+                    moveDir = moveDirX;
+                }
+                else
+                {
+                    var moveDirZ = new Vector3(0, 0, inputVector.y).normalized;
+                    canMove = !Physics.CapsuleCast(
+                        transform.position,
+                        transform.position + Vector3.up * playerHeight,
+                        playerRadius,
+                        moveDirZ,
+                        moveDistance);
+                    if (canMove)
+                    {
+                        moveDir = moveDirZ;
+                    }
+                    else
+                    {
+                    }
+                }
+            }
+
+            if (canMove)
+            {
+                transform.position += moveDir * (moveSpeed * Time.deltaTime);
+            }
+
+            IsWalking = moveDir != Vector3.zero;
             transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
         }
 
