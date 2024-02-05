8d03f3e03746525ec2ad81d4284536c2ff725633
vonbrank <vonbrank@outlook.com>
2024-02-05 14:58:30 +0800

    feat: 实现按键绑定功能

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -7,11 +7,24 @@ namespace Managers
 {
     public class InputManager : MonoBehaviour
     {
+        private const string PLAYER_PREFS_BIDINGS = "InputBindings";
+
         public static InputManager Instance { get; private set; }
-        
+
         public event EventHandler OnInteractAction;
         public event EventHandler OnInteractAlternateAction;
         public event EventHandler OnPauseAction;
+
+        public enum Binding
+        {
+            MoveUp,
+            MoveDown,
+            MoveLeft,
+            MoveRight,
+            Interact,
+            InteractAlt,
+            Pause,
+        }
 
         private PlayerInputActions playerInputActions;
 
@@ -25,8 +38,13 @@ namespace Managers
             }
 
             Instance = this;
-            
+
             playerInputActions = new PlayerInputActions();
+
+            if (PlayerPrefs.HasKey(PLAYER_PREFS_BIDINGS))
+            {
+                playerInputActions.LoadBindingOverridesFromJson(PlayerPrefs.GetString(PLAYER_PREFS_BIDINGS));
+            }
         }
 
         private void OnEnable()
@@ -77,6 +95,63 @@ namespace Managers
             inputVector.Normalize();
 
             return inputVector;
+        }
+
+        public string GetBindingText(Binding binding)
+        {
+            switch (binding)
+            {
+                default:
+
+                case Binding.MoveUp:
+                    return playerInputActions.Player.Move.bindings[1].ToDisplayString();
+                case Binding.MoveDown:
+                    return playerInputActions.Player.Move.bindings[2].ToDisplayString();
+                case Binding.MoveLeft:
+                    return playerInputActions.Player.Move.bindings[3].ToDisplayString();
+                case Binding.MoveRight:
+                    return playerInputActions.Player.Move.bindings[4].ToDisplayString();
+                case Binding.Interact:
+                    return playerInputActions.Player.Interact.bindings[0].ToDisplayString();
+                case Binding.InteractAlt:
+                    return playerInputActions.Player.InteractAlternate.bindings[0].ToDisplayString();
+                case Binding.Pause:
+                    return playerInputActions.Player.Pause.bindings[0].ToDisplayString();
+            }
+        }
+
+        public void RebindBinding(Binding binding, Action onRebindComplete)
+        {
+            (InputAction, int) inputActionAndBindingIndex = binding switch
+            {
+                Binding.MoveUp => (playerInputActions.Player.Move, 1),
+                Binding.MoveDown => (playerInputActions.Player.Move, 2),
+                Binding.MoveLeft => (playerInputActions.Player.Move, 3),
+                Binding.MoveRight => (playerInputActions.Player.Move, 4),
+                Binding.Interact => (playerInputActions.Player.Interact, 0),
+                Binding.InteractAlt => (playerInputActions.Player.InteractAlternate, 0),
+                Binding.Pause => (playerInputActions.Player.Pause, 0),
+                _ => (null, 0)
+            };
+
+            if (inputActionAndBindingIndex.Item1 is null)
+            {
+                return;
+            }
+
+            playerInputActions.Player.Disable();
+
+            inputActionAndBindingIndex.Item1.PerformInteractiveRebinding(inputActionAndBindingIndex.Item2)
+                .OnComplete(callback =>
+                {
+                    playerInputActions.Player.Enable();
+                    callback.Dispose();
+
+                    onRebindComplete?.Invoke();
+
+                    PlayerPrefs.SetString(PLAYER_PREFS_BIDINGS, playerInputActions.SaveBindingOverridesAsJson());
+                    PlayerPrefs.Save();
+                }).Start();
         }
     }
 }
--- Assets/Scripts/UI/OptionsUI.cs
+++ Assets/Scripts/UI/OptionsUI.cs
@@ -11,14 +11,39 @@ namespace UI
         [SerializeField] private Button soundEffectsButton;
         [SerializeField] private Button musicButton;
         [SerializeField] private Button closeButton;
+        [SerializeField] private Button moveUpButton;
+        [SerializeField] private Button moveDownButton;
+        [SerializeField] private Button moveLeftButton;
+        [SerializeField] private Button moveRightButton;
+        [SerializeField] private Button interactButton;
+        [SerializeField] private Button interactAltButton;
+        [SerializeField] private Button pauseButton;
+
         [SerializeField] private TextMeshProUGUI soundEffectsText;
         [SerializeField] private TextMeshProUGUI musicText;
+        [SerializeField] private TextMeshProUGUI moveUpText;
+        [SerializeField] private TextMeshProUGUI moveDownText;
+        [SerializeField] private TextMeshProUGUI moveLeftText;
+        [SerializeField] private TextMeshProUGUI moveRightText;
+        [SerializeField] private TextMeshProUGUI interactText;
+        [SerializeField] private TextMeshProUGUI interactAltText;
+        [SerializeField] private TextMeshProUGUI pauseText;
+
+        [SerializeField] private Transform pressToRebindKeyGameObject;
 
         private void OnEnable()
         {
             soundEffectsButton.onClick.AddListener(HandleSoundEffectsButtonClick);
             musicButton.onClick.AddListener(HandleMusicButtonClick);
             closeButton.onClick.AddListener(HandleCloseButtonClick);
+
+            moveUpButton.onClick.AddListener(HandleMoveUpButtonClick);
+            moveDownButton.onClick.AddListener(HandleMoveDownButtonClick);
+            moveLeftButton.onClick.AddListener(HandleMoveLeftButtonClick);
+            moveRightButton.onClick.AddListener(HandleMoveRightButtonClick);
+            interactButton.onClick.AddListener(HandleInteractButtonClick);
+            interactAltButton.onClick.AddListener(HandleInteractAltButtonClick);
+            pauseButton.onClick.AddListener(HandlePauseButtonClick);
         }
 
         private void OnDisable()
@@ -26,12 +51,20 @@ namespace UI
             soundEffectsButton.onClick.RemoveListener(HandleSoundEffectsButtonClick);
             musicButton.onClick.RemoveListener(HandleMusicButtonClick);
             closeButton.onClick.RemoveListener(HandleCloseButtonClick);
+
+            moveUpButton.onClick.RemoveListener(HandleMoveUpButtonClick);
+            moveDownButton.onClick.RemoveListener(HandleMoveDownButtonClick);
+            moveLeftButton.onClick.RemoveListener(HandleMoveLeftButtonClick);
+            moveRightButton.onClick.RemoveListener(HandleMoveRightButtonClick);
+            interactButton.onClick.RemoveListener(HandleInteractButtonClick);
+            interactAltButton.onClick.RemoveListener(HandleInteractAltButtonClick);
+            pauseButton.onClick.RemoveListener(HandlePauseButtonClick);
         }
 
         private void Start()
         {
             UpdateVisual();
-            
+
             Hide();
         }
 
@@ -56,9 +89,18 @@ namespace UI
         {
             soundEffectsText.text = $"Sound Effects: {Mathf.RoundToInt(SoundManager.Instance.VolumeMultiplier * 10)}";
             musicText.text = $"Music: {Mathf.RoundToInt(MusicManager.Instance.VolumeMultiplier * 10)}";
+
+            moveUpText.text = InputManager.Instance.GetBindingText(InputManager.Binding.MoveUp);
+            moveDownText.text = InputManager.Instance.GetBindingText(InputManager.Binding.MoveDown);
+            moveLeftText.text = InputManager.Instance.GetBindingText(InputManager.Binding.MoveLeft);
+            moveRightText.text = InputManager.Instance.GetBindingText(InputManager.Binding.MoveRight);
+            interactText.text = InputManager.Instance.GetBindingText(InputManager.Binding.Interact);
+            interactAltText.text = InputManager.Instance.GetBindingText(InputManager.Binding.InteractAlt);
+            pauseText.text = InputManager.Instance.GetBindingText(InputManager.Binding.Pause);
         }
 
         [SerializeField] private Transform[] visualGameObjects;
+
 
         public void Show()
         {
@@ -74,6 +116,38 @@ namespace UI
             {
                 visualTransform.gameObject.SetActive(false);
             }
+
+            HidePressToRebindKey();
+        }
+
+        private void ShowPressToRebindKey()
+        {
+            pressToRebindKeyGameObject.gameObject.SetActive(true);
+        }
+
+        private void HidePressToRebindKey()
+        {
+            pressToRebindKeyGameObject.gameObject.SetActive(false);
+        }
+
+        private void HandleRebindComplete()
+        {
+            UpdateVisual();
+            HidePressToRebindKey();
+        }
+
+        private void HandleMoveUpButtonClick() => RebindBinding(InputManager.Binding.MoveUp);
+        private void HandleMoveDownButtonClick() => RebindBinding(InputManager.Binding.MoveDown);
+        private void HandleMoveLeftButtonClick() => RebindBinding(InputManager.Binding.MoveLeft);
+        private void HandleMoveRightButtonClick() => RebindBinding(InputManager.Binding.MoveRight);
+        private void HandleInteractButtonClick() => RebindBinding(InputManager.Binding.Interact);
+        private void HandleInteractAltButtonClick() => RebindBinding(InputManager.Binding.InteractAlt);
+        private void HandlePauseButtonClick() => RebindBinding(InputManager.Binding.Pause);
+
+        private void RebindBinding(InputManager.Binding binding)
+        {
+            ShowPressToRebindKey();
+            InputManager.Instance.RebindBinding(binding, HandleRebindComplete);
         }
     }
 }
